d2ee53934ed4932ec2726b70c07552063bbbf2b1
Amrock99 <kohliaman246@gmail.com>
2022-09-25 13:06:09 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,53 +1,66 @@
 import "./App.css";
 import Coin from "./components/Coin";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import ClipLoader from "react-spinners/ClipLoader";
 
-async function getData(){
-  const res = await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false");
-  return await res;
+
+async function getData() {
+  const res = await axios.get(
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+  );
+  return res;
 }
 
 function App() {
-    
-    const [Coins,setCoins] = useState([]);
-    const [searchWord, setSearchWord] = useState("");
-    
+  const [Coins, setCoins] = useState([]);
+  const [searchWord, setSearchWord] = useState("");
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
     setTimeout(() => {
-        getData().then((res) => {
-            setCoins(res.data);
-        });
+      getData().then((res) => {
+        setCoins(res.data);
+        setLoading(false);
+      });
     }, 1000);
-    // useEffect(() => {
-    //   axios
-    //   .get(
-    //     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-    //     )
-    //     .then((res) => {
-    //       setCoins(res.data);
-    //     });
-    //   }, []);
-      
+  }, []);
+
   const filteredCoins = Coins.filter((coin) => {
     return coin.name.toLowerCase().includes(searchWord.toLowerCase());
   });
 
-
   return (
     <div className="App">
-      <div className="cryptoHeader">
-      <input
-          type="text"
-          placeholder="Bitcoin..."
-          onChange={(event) => {
-            setSearchWord(event.target.value);
-          }}
-        />
-      </div>
-      <div className="coinDisplay">
-      {filteredCoins.map((coin) => {
-      return <Coin name={coin.name} image={coin.image} current_price={coin.current_price} symbol={coin.symbol} high={coin.high_24h} low={coin.low_24h}/>})}
-      </div>
+      {loading ? (
+        <ClipLoader className="Loader" color={"#ffb100"} loading={loading} size={50} margin={10}/>
+      ) : (
+        <>
+          <div className="cryptoHeader">
+            <input
+              type="text"
+              placeholder="Bitcoin..."
+              onChange={(event) => {
+                setSearchWord(event.target.value);
+              }}
+            />
+          </div>
+          <div className="coinDisplay">
+            {filteredCoins.map((coin) => {
+              return (
+                <Coin
+                  name={coin.name}
+                  image={coin.image}
+                  current_price={coin.current_price}
+                  symbol={coin.symbol}
+                  high={coin.high_24h}
+                  low={coin.low_24h}
+                />
+              );
+            })}
+          </div>
+        </>
+      )}
     </div>
   );
 }
